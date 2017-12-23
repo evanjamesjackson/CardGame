e96e859e6efda7ef3fb21dd6e80722d8a73ce75e
evanjamesjackson <evanjamesjackson@gmail.com>
2017-12-22 21:06:15 -0400

    Added useful static properties. Added addCard method. Javadoc.

--- src/blackjack/BlackjackHand.java
+++ src/blackjack/BlackjackHand.java
@@ -5,11 +5,31 @@ import card.Face;
 import card.Hand;
 
 public class BlackjackHand extends Hand {
+	/**
+	 * Value for a perfect hand.
+	 */
+	public static final int PERFECT_HAND = 21;
+	
+	/**
+	 * Number of cards a hand contains at the start of a game.
+	 */
+	public static final int INITIAL_NUM_CARDS = 2;
+	
+	public static final int MAX_NUM_CARDS = 5;
+	
 	private static final int HIGH_ACE_VALUE = 11; 
-	private static final int PERFECT_HAND = 21;
 	
 	public BlackjackHand() {
 		super();
+	}
+	
+	@Override
+	public void addCard(Card toAdd) {
+		if (getNumCards() <= MAX_NUM_CARDS) {
+			super.addCard(toAdd);
+		} else {
+			throw new MaxCardsExceededException();
+		}
 	}
 	
 	/**
@@ -39,6 +59,9 @@ public class BlackjackHand extends Hand {
 		return reducedValue;
 	}
 
+	/**
+	 * Returns the number of Aces in this hand.
+	 */
 	private int countAces() {
 		int numAces = 0;
 		for (Card card : this.CARDS) {
@@ -47,5 +70,9 @@ public class BlackjackHand extends Hand {
 			}
 		}
 		return numAces;
+	}
+	
+	public class MaxCardsExceededException extends IllegalArgumentException {
+		private static final long serialVersionUID = 8047540573063045375L;
 	}
 }
